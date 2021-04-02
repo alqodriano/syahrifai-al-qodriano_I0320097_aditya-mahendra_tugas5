507a5437c655f00650bcaf113dbd1184901579a9
alqodriano <80144761+alqodriano@users.noreply.github.com>
2021-04-03 00:31:52 +0700

    Add files via upload

---
+++ I0320097_soal1_tugas5 .py
@@ -0,0 +1,8 @@
+#soal1
+nama_anda = input("Masukkan nama anda: ")
+jenis_kelamin = input("Masukkan jenis kelamin: ")
+if (jenis_kelamin=="laki"):
+    print("Selamat Datang"+","+" "+"Tuan"+" "+str(nama_anda)+"!")
+else:
+    print("Selamat Datang"+","+" "+"Nyonya"+" "+str(nama_anda)+"!")
+print()
